c54a5c2178d976da2c2815f8406fe0a4ef7fbccf
Sherlyn Burkhart <SherlynKBurkhart@users.noreply.github.com>
2015-09-28 11:39:17 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-//exampleFile-406-FacebookViewer-server.js
-//exampleFile-325-mini-birds-models-server.js
-//exampleFile-GITHUB-JASON DAWSON POLL SERVER.JS, has googleStrategy also
-//exampleFile-GITHUB-JARED HANSON'S PASSPORT-FACEBOOK  
-
 // Dependencies
 var bodyParser = require('body-parser'), 
 	cool = require('cool-ascii-faces'),
@@ -21,13 +16,12 @@ var bodyParser = require('body-parser'),
 // Facebook info	
 	facebookStrategy = require('passport-facebook').Strategy,
 	localStrategy = require('passport-local').Strategy,
-	facebookAppId = "1503215263303896",
-	facebookAppSecret = "8e1684e7eff2add24e0d164016996655";
+	facebookAppId = "",
+	facebookAppSecret = "";
 
 // Connections
 var port = 9005;
 var mongoUri = 'mongodb://localhost:27017/KasallisDesignStudio';
-
 
 // Login user 
 passport.serializeUser(function(user, done) {
@@ -49,15 +43,10 @@ passport.use(new facebookStrategy({
 	enableProof: false
 },	
 	function(accessToken, refreshToken, profile, done) {
-		// userCtrl.findOrCreate({ facebookId: profile.id },
-		// 	function (err, user) {
-		// 	}
-		//);  				
+			
 		return done();
 	})
 );
-
-
 
 // Middleware
 var	app = express();
@@ -87,35 +76,7 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
-//THIS COMES FROM JASONDAWSON POLL SERVER.JS
-//DO I USE? WHY?
-// // Add headers
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-  
-//     next();
-// });
-
-
-// Endpoints
-// app.get('/', 
-// 	function(req, res){
-// 	res.render('index', { user: req.user });
-// });
-
-// app.get('/account', ensureAuthenticated, 
-// 	function(req, res){
-// 	res.render('account', { user: req.user });
-// });
-
-// app.get('/login', 
-// 	function(req, res){
-// 	res.render('login', { user: req.user });
-// });
+: req.user });
 
 app.get('/auth/facebook', 
 	passport.authenticate('facebook')
@@ -125,12 +86,6 @@ app.get('/auth/facebook/callback',
 	passport.authenticate('facebook',
 	{ successRedirect: '/', failureRedirect: '/' }),
 	function(req, res) {
-		// //THIS WAS IN JASONDAWSON POLL
-		//DO I NEED?
-		// user.getUser(req.user.id).then(
-		// function(userProfile) {
-  //   		currentUser = userProfile;
-  //   	})
 	res.redirect('/');
 });
 
@@ -138,44 +93,12 @@ app.get('auth/logout',
 	function(req, res){
 		req.logout();
 		res.redirect('/');
-		// OR FROM JASOND POLL
-		//currentUser = ();
-		//res.status(200).end();
-		//REPLACES res.redirect('/');
 });
-
-// //FROM JASONDAWSON POLL
-// I HAVE NOTHING LIKE THIS, DO I NEED IT?
-// app.get('/auth/currentUser', function(req, res) {
-//   if (req.user) {
-//   User.getUser(req.user.id).then(function(response) {
-//       console.log('currentUser api check:');
-//       console.log(req.user.name);
-//       res.status(200).json(response);
-//     });
-//   } else {
-//     res.end();
-//   }
-// });
-
-//I ALSO HAVE DIFFERENT ENDPOINTS THAN JASONDAWSON
-
 
 function ensureAuthenticated(req, res, next) {
 	if (req.isAuthenticated()) { return next(); }
 	res.redirect('/login')
 };
-
-//AND...DO I NEED THESE?
-// app.post('/user', userCtrl.create);
-// app.get('/user', userCtrl.read);
-// app.put('/user/:id', userCtrl.update);
-// app.delete('/user/:id', userCtrl.delete);
-
-// app.post('/purchases', purchaseCtrl.create);
-// app.get('/purchases', purchasesCtrl.read);
-// app.put('/purchases/:id', purchasesCtrl.update);
-// app.delete('/purchases/:id', purchasesCtrl.delete);
 
 app.get('api/me', function(req, res){
 	console.log(333333, req.user);
@@ -188,11 +111,6 @@ app.get('api/session', function(req, res){
 	res.send('check the session');
 });
 
-// mongoose.connect(mongoUri);
-// mongoose.connection.once('open', function() {
-// 	console.log('Connected to MongoDB at ', mongoUri);
-// });
-// //THIS IS JASONDAWSON'S CODE
 mongoose.connect(mongoUri);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -203,6 +121,3 @@ db.once('open', function() {
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port ', app.get('port'));
 }); 
-// Use Postman to verify that you can in fact 
-//get the JSON data from the `/me` endpoint.
-
